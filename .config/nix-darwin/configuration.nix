1d2c2f67f42c1405cab3407f9ba5ad39f88f386d
Edu Paz <jooeduardo8@gmail.com>
2025-01-04 22:36:56 -0300

    move aerospace and others

--- .config/nix-darwin/configuration.nix
+++ .config/nix-darwin/configuration.nix
@@ -2,15 +2,16 @@
 {
   # DO NOT CHANGE
 
-  imports = [ <home-manager/nix-os> <home-manager/nix-darwin> ];
+  imports = [
+    ../nix-shared
+    ./aerospace.nix
+    <home-manager/nix-darwin>
+  ];
 
   environment.darwinConfig = "$HOME/.config/nix-darwin/configuration.nix";
   system.stateVersion = 5;
 
   # Settings
-
-  home-manager.useGlobalPkgs = true;
-  nixpkgs.config.allowUnfree = true;
 
   ## Homebrew settings
 
@@ -39,26 +40,6 @@
   system.defaults.loginwindow.autoLoginUser = "eduardo.paz";
   time.timeZone = "America/Sao_Paulo";
 
-  # Aliases
-
-  environment.shellAliases = {
-    b = "bat";
-    v = "vim";
-
-    cp = "cp -vi";
-    ln = "ln -vi";
-    mv = "mv -vi";
-    rm = "rm -vi";
-
-    gl = "git pull";
-    gagc = "git add . && git commit";
-    gp = "git push";
-    gs = "git status";
-    grhforno = "git reset --hard origin/forno";
-    grhmain = "git reset --hard origin/main";
-    grhmaster = "git reset --hard origin/master";
-  };
-
   # Env vars
 
   environment.variables = {
@@ -70,104 +51,6 @@
   # Fonts
 
   fonts.packages = [ (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; }) ];
-
-  # Aerospace
-
-  services.aerospace.enable = true;
-  services.aerospace.settings.on-focused-monitor-changed = [
-    "move-mouse monitor-lazy-center"
-  ];
-  services.aerospace.settings.on-focus-changed = [
-    "move-mouse monitor-lazy-center"
-  ];
-
-  services.aerospace.settings.accordion-padding = 0;
-
-  services.aerospace.settings.gaps.inner.horizontal = 5;
-  services.aerospace.settings.gaps.inner.vertical = 5;
-
-  services.aerospace.settings.gaps.outer.left = 5;
-  services.aerospace.settings.gaps.outer.bottom = 5;
-  services.aerospace.settings.gaps.outer.top = 5;
-  services.aerospace.settings.gaps.outer.right = 5;
-
-  services.aerospace.settings.on-window-detected = [
-    {
-      check-further-callbacks = true;
-      "if".app-id = "com.postmanlabs.mac";
-      run = [ "move-node-to-workspace D" ];
-    }
-    {
-      check-further-callbacks = true;
-      "if".app-id = "org.mozilla.firefox";
-      run = [ "move-node-to-workspace D" ];
-    }{
-      "if".app-id = "com.jetbrains.intellij";
-      run = [ "move-node-to-workspace D" ];
-    }
-  ];
-
-  services.aerospace.settings.mode.main.binding.alt-slash = "layout tiles horizontal vertical";
-  services.aerospace.settings.mode.main.binding.alt-comma = "layout accordion horizontal";
-
-  services.aerospace.settings.mode.main.binding.alt-h = "focus left";
-  services.aerospace.settings.mode.main.binding.alt-j = "focus down";
-  services.aerospace.settings.mode.main.binding.alt-k = "focus up";
-  services.aerospace.settings.mode.main.binding.alt-l = "focus right";
-
-  services.aerospace.settings.mode.main.binding.alt-shift-h = "move left";
-  services.aerospace.settings.mode.main.binding.alt-shift-j = "move down";
-  services.aerospace.settings.mode.main.binding.alt-shift-k = "move up";
-  services.aerospace.settings.mode.main.binding.alt-shift-l = "move right";
-
-  services.aerospace.settings.mode.main.binding.alt-shift-minus = "resize smart -50";
-  services.aerospace.settings.mode.main.binding.alt-shift-equal = "resize smart +50";
-
-  services.aerospace.settings.mode.main.binding.alt-1 = "workspace 1";
-  services.aerospace.settings.mode.main.binding.alt-2 = "workspace 2";
-  services.aerospace.settings.mode.main.binding.alt-3 = "workspace 3";
-  services.aerospace.settings.mode.main.binding.alt-4 = "workspace 4";
-  services.aerospace.settings.mode.main.binding.alt-5 = "workspace 5";
-  services.aerospace.settings.mode.main.binding.alt-d = "workspace D";
-
-  services.aerospace.settings.mode.main.binding.alt-shift-1 = "move-node-to-workspace 1";
-  services.aerospace.settings.mode.main.binding.alt-shift-2 = "move-node-to-workspace 2";
-  services.aerospace.settings.mode.main.binding.alt-shift-3 = "move-node-to-workspace 3";
-  services.aerospace.settings.mode.main.binding.alt-shift-4 = "move-node-to-workspace 4";
-  services.aerospace.settings.mode.main.binding.alt-shift-5 = "move-node-to-workspace 5";
-  services.aerospace.settings.mode.main.binding.alt-shift-d = "move-node-to-workspace D";
-
-  services.aerospace.settings.mode.main.binding.alt-tab = "workspace-back-and-forth";
-  services.aerospace.settings.mode.main.binding.alt-shift-tab =
-    "move-workspace-to-monitor --wrap-around next";
-
-  services.aerospace.settings.mode.main.binding.alt-shift-semicolon = "mode service";
-
-  services.aerospace.settings.mode.service.binding.esc = [
-    "reload-config"
-    "mode main"
-  ];
-  services.aerospace.settings.mode.service.binding.f = [
-    "layout floating tiling"
-    "mode main"
-  ];
-
-  services.aerospace.settings.mode.service.binding.alt-shift-h = [
-    "join-with left"
-    "mode main"
-  ];
-  services.aerospace.settings.mode.service.binding.alt-shift-j = [
-    "join-with down"
-    "mode main"
-  ];
-  services.aerospace.settings.mode.service.binding.alt-shift-k = [
-    "join-with up"
-    "mode main"
-  ];
-  services.aerospace.settings.mode.service.binding.alt-shift-l = [
-    "join-with right"
-    "mode main"
-  ];
 
   # Packages
 
---
+++ .config/nix-shared/default.nix
@@ -0,0 +1,27 @@
+{
+  # Settings  
+
+  nixpkgs.config.allowUnfree = true;
+  home-manager.useGlobalPkgs = true;
+
+  # Aliases
+
+  environment.shellAliases = {
+    b = "bat";
+    v = "vim";
+
+    cp = "cp -vi";
+    ln = "ln -vi";
+    mv = "mv -vi";
+    rm = "rm -vi";
+
+    ga = "git add .";
+    gc = "git commit";
+    gl = "git pull";
+    gp = "git push";
+    gs = "git status";
+    grhforno = "git reset --hard origin/forno";
+    grhmain = "git reset --hard origin/main";
+    grhmaster = "git reset --hard origin/master";
+  };
+}
