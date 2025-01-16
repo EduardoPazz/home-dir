66376d35bc7cebff2781520c0fe3caeb58c6d849
Edu Paz <jooeduardo8@gmail.com>
2025-01-16 13:55:13 -0300

    Revert "reenable atuin up-arrow"
    This reverts commit 0a97859ad2b3cb40608c38bd4b31237877d2dc75.

--- .config/nix-darwin/configuration.nix
+++ .config/nix-darwin/configuration.nix
@@ -111,11 +111,14 @@ in
       programs = {
         mise.enable = true;
 
-        atuin.enable = true;
-
         starship.settings.character = {
           success_symbol = "[󰀵](bold green)";
           error_symbol = "[󰀵](bold red)";
+        };
+
+        atuin = {
+          enable = true;
+          flags = [ "--disable-up-arrow" ];
         };
 
         kitty = {
